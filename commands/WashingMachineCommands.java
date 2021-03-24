dcbc23bc3d8597714d143f4274607dad8c6e2bd8
TNT3102 <80429140+TNT3102@users.noreply.github.com>
2021-03-24 17:26:12 -0500

    Add files via upload

---
+++ commands/WashingMachineCommands.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.WashingMachine;
+
+public class WashingMachineCommands extends CommandBase {
+  private WashingMachine m_wMachine;
+  /** Creates a new Washing. */
+  public WashingMachineCommands(WashingMachine subsytem) {
+    m_wMachine = subsytem;
+    addRequirements(m_wMachine);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_wMachine.set(1.0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_wMachine.set(0.0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
